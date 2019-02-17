7e4f08d45350f97c42786e40f86ec846b1f3d5be
Discordian <therealdiscordian@gmail.com>
2019-02-17 12:51:38 -0500

    First commit.

---
+++ TurboHUDManager/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace TurboHUDManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.EnabledCheckBox = new System.Windows.Forms.CheckBox();
+            this.EnabledLabel = new System.Windows.Forms.Label();
+            this.NameDisplay = new System.Windows.Forms.Label();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.AuthorDisplay = new System.Windows.Forms.Label();
+            this.AuthorLabel = new System.Windows.Forms.Label();
+            this.IdDisplay = new System.Windows.Forms.Label();
+            this.IdLabel = new System.Windows.Forms.Label();
+            this.PluginListBox = new System.Windows.Forms.ListBox();
+            this.AuthorListBox = new System.Windows.Forms.ListBox();
+            this.PathBtn = new System.Windows.Forms.Button();
+            this.PathBox = new System.Windows.Forms.TextBox();
+            this.PathBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.EnabledCheckBox);
+            this.panel1.Controls.Add(this.EnabledLabel);
+            this.panel1.Controls.Add(this.NameDisplay);
+            this.panel1.Controls.Add(this.NameLabel);
+            this.panel1.Controls.Add(this.AuthorDisplay);
+            this.panel1.Controls.Add(this.AuthorLabel);
+            this.panel1.Controls.Add(this.IdDisplay);
+            this.panel1.Controls.Add(this.IdLabel);
+            this.panel1.Controls.Add(this.PluginListBox);
+            this.panel1.Controls.Add(this.AuthorListBox);
+            this.panel1.Controls.Add(this.PathBtn);
+            this.panel1.Controls.Add(this.PathBox);
+            this.panel1.ForeColor = System.Drawing.Color.Red;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(749, 370);
+            this.panel1.TabIndex = 0;
+            // 
+            // EnabledCheckBox
+            // 
+            this.EnabledCheckBox.AutoSize = true;
+            this.EnabledCheckBox.Enabled = false;
+            this.EnabledCheckBox.Location = new System.Drawing.Point(464, 74);
+            this.EnabledCheckBox.Name = "EnabledCheckBox";
+            this.EnabledCheckBox.Size = new System.Drawing.Size(15, 14);
+            this.EnabledCheckBox.TabIndex = 11;
+            this.EnabledCheckBox.UseVisualStyleBackColor = true;
+            this.EnabledCheckBox.Click += new System.EventHandler(this.EnabledCheckBox_Click);
+            // 
+            // EnabledLabel
+            // 
+            this.EnabledLabel.AutoSize = true;
+            this.EnabledLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EnabledLabel.Location = new System.Drawing.Point(407, 75);
+            this.EnabledLabel.Name = "EnabledLabel";
+            this.EnabledLabel.Size = new System.Drawing.Size(57, 13);
+            this.EnabledLabel.TabIndex = 10;
+            this.EnabledLabel.Text = "Enabled:";
+            // 
+            // NameDisplay
+            // 
+            this.NameDisplay.AutoSize = true;
+            this.NameDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameDisplay.Location = new System.Drawing.Point(461, 58);
+            this.NameDisplay.Name = "NameDisplay";
+            this.NameDisplay.Size = new System.Drawing.Size(27, 13);
+            this.NameDisplay.TabIndex = 9;
+            this.NameDisplay.Text = "N/A";
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameLabel.Location = new System.Drawing.Point(407, 58);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(43, 13);
+            this.NameLabel.TabIndex = 8;
+            this.NameLabel.Text = "Name:";
+            // 
+            // AuthorDisplay
+            // 
+            this.AuthorDisplay.AutoSize = true;
+            this.AuthorDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AuthorDisplay.Location = new System.Drawing.Point(461, 45);
+            this.AuthorDisplay.Name = "AuthorDisplay";
+            this.AuthorDisplay.Size = new System.Drawing.Size(27, 13);
+            this.AuthorDisplay.TabIndex = 7;
+            this.AuthorDisplay.Text = "N/A";
+            // 
+            // AuthorLabel
+            // 
+            this.AuthorLabel.AutoSize = true;
+            this.AuthorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AuthorLabel.Location = new System.Drawing.Point(407, 45);
+            this.AuthorLabel.Name = "AuthorLabel";
+            this.AuthorLabel.Size = new System.Drawing.Size(48, 13);
+            this.AuthorLabel.TabIndex = 6;
+            this.AuthorLabel.Text = "Author:";
+            // 
+            // IdDisplay
+            // 
+            this.IdDisplay.AutoSize = true;
+            this.IdDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IdDisplay.Location = new System.Drawing.Point(461, 32);
+            this.IdDisplay.Name = "IdDisplay";
+            this.IdDisplay.Size = new System.Drawing.Size(27, 13);
+            this.IdDisplay.TabIndex = 5;
+            this.IdDisplay.Text = "N/A";
+            // 
+            // IdLabel
+            // 
+            this.IdLabel.AutoSize = true;
+            this.IdLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IdLabel.Location = new System.Drawing.Point(407, 32);
+            this.IdLabel.Name = "IdLabel";
+            this.IdLabel.Size = new System.Drawing.Size(28, 13);
+            this.IdLabel.TabIndex = 4;
+            this.IdLabel.Text = "ID: ";
+            // 
+            // PluginListBox
+            // 
+            this.PluginListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.PluginListBox.FormattingEnabled = true;
+            this.PluginListBox.Location = new System.Drawing.Point(157, 33);
+            this.PluginListBox.Name = "PluginListBox";
+            this.PluginListBox.Size = new System.Drawing.Size(244, 329);
+            this.PluginListBox.Sorted = true;
+            this.PluginListBox.TabIndex = 3;
+            this.PluginListBox.SelectedIndexChanged += new System.EventHandler(this.PluginListBox_SelectedIndexChanged);
+            // 
+            // AuthorListBox
+            // 
+            this.AuthorListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.AuthorListBox.FormattingEnabled = true;
+            this.AuthorListBox.Location = new System.Drawing.Point(4, 33);
+            this.AuthorListBox.Name = "AuthorListBox";
+            this.AuthorListBox.Size = new System.Drawing.Size(147, 329);
+            this.AuthorListBox.Sorted = true;
+            this.AuthorListBox.TabIndex = 2;
+            this.AuthorListBox.SelectedIndexChanged += new System.EventHandler(this.AuthorListBox_SelectedIndexChanged);
+            // 
+            // PathBtn
+            // 
+            this.PathBtn.CausesValidation = false;
+            this.PathBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PathBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.PathBtn.Location = new System.Drawing.Point(4, 3);
+            this.PathBtn.Name = "PathBtn";
+            this.PathBtn.Size = new System.Drawing.Size(112, 23);
+            this.PathBtn.TabIndex = 1;
+            this.PathBtn.Text = "THUD Plugins Path";
+            this.PathBtn.UseVisualStyleBackColor = true;
+            this.PathBtn.Click += new System.EventHandler(this.PathBtn_Click);
+            // 
+            // PathBox
+            // 
+            this.PathBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PathBox.Location = new System.Drawing.Point(122, 3);
+            this.PathBox.Name = "PathBox";
+            this.PathBox.Size = new System.Drawing.Size(624, 20);
+            this.PathBox.TabIndex = 0;
+            this.PathBox.TextChanged += new System.EventHandler(this.PathBox_TextChanged);
+            // 
+            // PathBrowser
+            // 
+            this.PathBrowser.ShowNewFolderButton = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(749, 370);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(765, 400);
+            this.Name = "Form1";
+            this.Text = "TurboHUD Plugin Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button PathBtn;
+        private System.Windows.Forms.TextBox PathBox;
+        private System.Windows.Forms.FolderBrowserDialog PathBrowser;
+        private System.Windows.Forms.CheckBox EnabledCheckBox;
+        private System.Windows.Forms.Label EnabledLabel;
+        private System.Windows.Forms.Label NameDisplay;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Label AuthorDisplay;
+        private System.Windows.Forms.Label AuthorLabel;
+        private System.Windows.Forms.Label IdDisplay;
+        private System.Windows.Forms.Label IdLabel;
+        private System.Windows.Forms.ListBox PluginListBox;
+        private System.Windows.Forms.ListBox AuthorListBox;
+    }
+}
+
---
+++ TurboHUDManager/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TurboHUDManager
+{
+    public partial class Form1 : Form
+    {
+        const string skeleton = @"// This file is managed by TurboHUD Plugin Manager, it is used as a database, DO NOT MODIFY.
+using Turbo.Plugins.Default;
+
+namespace Turbo.Plugins.User
+{{
+
+	public class PluginManagerPlugin : BasePlugin, ICustomizer
+	{{
+
+		public PluginManagerPlugin()
+		{{
+			Enabled = true;
+		}}
+
+		public override void Load(IController hud)
+		{{
+			base.Load(hud);
+		}}
+
+		public void Customize()
+		{{{0}
+		}}
+	}}
+}}";
+
+        public void SaveChanges()
+        {
+            string toggleLines = "";
+            string lastAuthor = "";
+            for (var i = 0; i < Plugins.Count; i++)
+            {
+                if (lastAuthor != Plugins[i].author)
+                {
+                    toggleLines = toggleLines + String.Format("\n\t\t\t//{0}", Plugins[i].author);
+                    lastAuthor = Plugins[i].author;
+                }
+                if (Plugins[i].author != "Default")
+                    toggleLines = toggleLines + String.Format("\n\t\t\tHud.TogglePlugin<{0}>({1});", Plugins[i].id, Plugins[i].enabled.ToString().ToLower());
+                else
+                    toggleLines = toggleLines + String.Format("\n\t\t\tHud.TogglePlugin<{0}>({1});", Plugins[i].name, Plugins[i].enabled.ToString().ToLower());
+            }
+            System.Diagnostics.Debug.WriteLine(toggleLines);
+            System.IO.File.WriteAllText(PathBox.Text + @"\User\PluginManagerPlugin.cs", String.Format(skeleton, toggleLines));
+        }
+
+        public void LoadChanges()
+        {
+            if (!File.Exists(PathBox.Text + @"\User\PluginManagerPlugin.cs"))
+                return;
+            var lines = File.ReadLines(PathBox.Text + @"\User\PluginManagerPlugin.cs", Encoding.UTF8).ToArray();
+            string author = "";
+            for (var i = 22; i < lines.Length-5; i++)
+            {
+                if (lines[i].IndexOf("//") > -1)
+                    author = lines[i].Substring(lines[i].IndexOf("//") + 1);
+
+                int index = lines[i].IndexOf("Hud.TogglePlugin<"); // Check if this line is a toggle line.
+                
+                if (index > -1)
+                {
+                    int pIndex = -1;
+                    if (author == "Default")
+                        pIndex = IndexPlugin(lines[i].Substring(index + 17, lines[i].IndexOf(">") - index - 17));
+                    else
+                        pIndex = IndexPluginID(lines[i].Substring(index + 17, lines[i].IndexOf(">") - index - 17));
+
+                    if (pIndex == -1)
+                    {
+                        continue;
+                    }
+                    bool pEnabled = lines[i].Substring(lines[i].IndexOf('(') + 1, 4) == "true";
+                    Plugins[pIndex].enabled = pEnabled;
+                }
+            }
+        }
+
+        // Object representing a plugin.
+        public class PluginObj
+        {
+            public string name, id, author, path;
+            public bool enabled;
+
+            public PluginObj(string pId, string pName, string pAuthor, bool pEnabled, string pPath)
+            {
+                name = pName;
+                id = pId;
+                author = pAuthor;
+                enabled = pEnabled;
+                path = pPath;
+            }
+        }
+
+        public List<PluginObj> Plugins; // List of all known plugins
+
+        public List<int> AuthorPlugins(string author)
+        {
+            var Out = new List<int>();
+            bool foundAuthor = false;
+
+            for (var i = 0; i < Plugins.Count; i++)
+            {
+                if (Plugins[i].author == author)
+                {
+                    if (!foundAuthor)
+                        foundAuthor = true;
+                    Out.Add(i);
+                    System.Diagnostics.Debug.WriteLine(author + " plugin id " + i.ToString());
+                } else if (foundAuthor)
+                {
+                    break;
+                }
+            }
+
+            return Out;
+        }
+
+        public int IndexPlugin(string name)
+        {
+            for (var i = 0; i < Plugins.Count; i++)
+                if (Plugins[i].name == name)
+                    return i;
+
+            return -1;
+        }
+
+        public int IndexPluginID(string id)
+        {
+            for (var i = 0; i < Plugins.Count; i++)
+                if (Plugins[i].id == id)
+                    return i;
+
+            return -1;
+        }
+
+        public void LoadAuthor(string author)
+        {
+            var pluginList = AuthorPlugins(author);
+            EnabledCheckBox.Enabled = false;
+            PluginListBox.Items.Clear();
+            if (pluginList.Count == 0)
+                return;
+
+            for (var i = 0; i < pluginList.Count; i++)
+            {
+                //if (Plugins[pluginList[i]].enabled)
+                    PluginListBox.Items.Add(Plugins[pluginList[i]].name);
+                //else
+                //    PluginListBox.Items.Add("!" + Plugins[pluginList[i]].name);
+            }
+            EnabledCheckBox.Enabled = true;
+            PluginListBox.SelectedIndex = 0;
+        }
+
+        public void LoadPlugin(string name)
+        {
+            int index = IndexPlugin(name);
+            IdDisplay.Text = Plugins[index].id;
+            AuthorDisplay.Text = Plugins[index].author;
+            NameDisplay.Text = Plugins[index].name;
+            EnabledCheckBox.Checked = Plugins[index].enabled;
+        }
+
+        // Refreshes the list of plugins
+        public void RefreshPlugins()
+        {
+
+            string path = PathBox.Text;
+            string lastAuthor = "";
+
+            // Exit if we're not looking at a plugin directory.
+            if (!Directory.Exists(path) || path.Split('\\').Last().ToLower() != "plugins")
+            {
+                return;
+            }
+            Plugins = new List<PluginObj>(); // Clear plugin list.
+            AuthorListBox.Items.Clear();
+            PluginListBox.Items.Clear();
+            Properties.Settings.Default.THUDPath = path; // Prepare to change default path.
+
+            foreach (string file in Directory.EnumerateFiles(path, "*.cs", SearchOption.AllDirectories))
+            {
+                var info = file.Substring(path.Length, file.Length - path.Length - 3).Split('\\'); // [author, ... path info ..., plugin name]
+                // Skip if first entry is blank.
+                int skip = 0;
+                if (info[0].Length == 0)
+                {
+                    skip = 1;
+                }
+                
+                if (info[skip] != "User") // Only check plugins not in "User" directory.
+                {
+                    bool enabled = false;
+                    bool notPlugin = true;
+
+                    // Search until Enabled line is found, and set enabled variable accordingly.
+                    foreach (string line in File.ReadLines(file, Encoding.UTF8))
+                    {
+                        // Skip if file doesn't use "BasePlugin" (not a plugin)
+                        if (line.IndexOf("class") > -1 && line.IndexOf("BasePlugin") == -1)
+                            break;
+
+                        int index = line.IndexOf("Enabled = "); // Check if this line is an "Enabled" line.
+
+                        // If line is found, set notPlugin to false (indicating we've found a plugin), and set enabled depending on setting in file.
+                        if (index > -1)
+                        {
+                            notPlugin = false;
+                            if (line.Substring(index+10, 4) == "true")
+                            {
+                                enabled = true;
+                            } else
+                            {
+                                enabled = false;
+                            }
+                            break;
+                        }
+                    }
+                    if (notPlugin) // Skip if file isn't a plugin
+                    {
+                        continue;
+                    }
+                    if (info[skip] != lastAuthor)
+                    {
+                        AuthorListBox.Items.Add(info[skip]);
+                        lastAuthor = info[skip];
+                    }
+
+                    // TEMP FIX FOR BUG: https://www.ownedcore.com/forums/showthread.php?t=778385&p=4014303
+                    if (info.Last() == "UiHiddenPortraitSkillBarPlugin")
+                        info[info.Length - 1] = "UiHiddenPlayerSkillBarPlugin";
+
+                    Plugins.Add(new PluginObj(String.Join(".", info.Skip(skip).ToArray()), info.Last(), info[skip], enabled, path)); // Add plugin to list.
+                    System.Diagnostics.Debug.WriteLine(Plugins.Last().author + ": " + Plugins.Last().id + " (Enabled: " + Plugins.Last().enabled.ToString() + ")"); // Dump some debugging info.
+                }
+            }
+
+            LoadChanges();
+
+            Properties.Settings.Default.Save(); // Save new default path.
+
+            AuthorListBox.SelectedIndex = 0;
+            if (AuthorListBox.Items.Count > 0)
+                LoadAuthor(AuthorListBox.Items[0].ToString());
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PathBox.Text = Properties.Settings.Default.THUDPath;
+            PathBrowser.SelectedPath = PathBox.Text;
+            //RefreshPlugins();
+        }
+
+        private void PathBtn_Click(object sender, EventArgs e)
+        {
+            var result = PathBrowser.ShowDialog(); // Show plugin directory selection dialogue.
+            if (result == System.Windows.Forms.DialogResult.OK) // If user pressed "OK", update plugin lists.
+            {
+                PathBox.Text = PathBrowser.SelectedPath; // Update displayed path text.
+                //RefreshPlugins();
+            }
+        }
+
+        private void PathBox_TextChanged(object sender, EventArgs e)
+        {
+            PathBrowser.SelectedPath = PathBox.Text;
+            RefreshPlugins();
+        }
+
+        private void AuthorListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadAuthor(AuthorListBox.Items[AuthorListBox.SelectedIndex].ToString());
+        }
+
+        private void PluginListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadPlugin(PluginListBox.Items[PluginListBox.SelectedIndex].ToString());
+        }
+
+        private void EnabledCheckBox_Click(object sender, EventArgs e)
+        {
+            Plugins[IndexPlugin(PluginListBox.Items[PluginListBox.SelectedIndex].ToString())].enabled = EnabledCheckBox.Checked;
+            SaveChanges();
+        }
+    }
+}
